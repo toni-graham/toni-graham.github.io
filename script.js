817e16af76de079c04440cb5f22564808fdb8d77
toni-graham <155112552+toni-graham@users.noreply.github.com>
2024-11-20 13:02:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('passwordForm').addEventListener('submit', function(even
     var correctPasswordMittens = "mittens92!";
 
     if (password === correctPasswordWhiskers || password === correctPasswordMittens) {
-        feedback.textContent = "Success! The flag is: FLAG{Alex_Johnson_Pet_Flag}";
+        feedback.textContent = "Success! The flag is: Alex_Johnson_Pet_Flag";
         feedback.style.color = 'green';
     } else if (password.length > 0) {
         feedback.textContent = "Incorrect password. Hint: The password relates to Alex's favorite pet and a key moment in his life. Take a guess!";
